59df1c04dea87e56d8e3d6e975191acbb039948e
Ryan Christopher Bahillo <christopherbahillo@gmail.com>
2022-07-14 13:45:26 +0800

    templates changes

--- website/base/urls.py
+++ website/base/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 
-
 urlpatterns = [
     path('', views.home, name='home'),
     path('app/', views.app, name='app')
--- website/base/views.py
+++ website/base/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -7,7 +8,8 @@ def home(request):
     context = {}
     return render(request, 'base/home.html', context)
 
-
+@login_required(login_url='login')
 def app(request):
     context = {}
     return render(request, 'base/app.html', context)
+        
