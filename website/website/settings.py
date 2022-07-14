b091fda1ff0e0ecd4b1be3a12606ae922bfb679f
Ryan Christopher Bahillo <christopherbahillo@gmail.com>
2022-07-14 13:48:18 +0800

    accounts app added

--- website/website/settings.py
+++ website/website/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'base.apps.BaseConfig'
+    # 'crispy_forms',
+    'base.apps.BaseConfig',
+    'accounts.apps.AccountsConfig'
 ]
 
 MIDDLEWARE = [
@@ -119,6 +121,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+
+# CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
