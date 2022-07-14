09369a252a0852cbb4216579b722e4d2eab069d6
Ryan Christopher Bahillo <christopherbahillo@gmail.com>
2022-07-14 18:24:27 +0800

    crispy forms installed

--- website/website/settings.py
+++ website/website/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    # 'crispy_forms',
+    'crispy_forms',
     'base.apps.BaseConfig',
     'accounts.apps.AccountsConfig'
 ]
@@ -122,7 +122,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
-# CRISPY_TEMPLATE_PACK = "bootstrap4"
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
