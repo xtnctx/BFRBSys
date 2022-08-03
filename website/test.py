5a648433649ff98428fb2074a751c8c837b55e33
Ryan Christopher Bahillo <christopherbahillo@gmail.com>
2022-08-03 11:36:22 +0800

    model to C header file

--- website/test.py
+++ website/test.py
@@ -99,3 +99,46 @@ history = model.fit(inputs_train, outputs_train, epochs=600, batch_size=1,
 
 # # print the predictions and the expected ouputs
 # print("predictions =\n", np.round(predictions, decimals=3))
+
+
+# Convert the model to the TensorFlow Lite format without quantization
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+converter.optimizations = [tf.lite.Optimize.OPTIMIZE_FOR_SIZE]
+tflite_model = converter.convert()
+
+def hex_to_c_array(hex_data, var_name):
+    c_str = ''
+
+    # Create header guard
+    c_str += '#ifdef ' + var_name.upper() + '_H\n'
+    c_str += '#define ' + var_name.upper() + '_H\n\n'
+
+    # Add array length at top of file
+    c_str += '\nunsigned int ' + var_name + '_len = ' + str(len(hex_data)) + ';\n'
+
+    # Declare C variable
+    c_str += 'unsigned char ' + var_name + '[] = {'
+    hex_array = []
+
+    for i, val in enumerate(hex_data):
+        # Construct string from hex
+        hex_str = format(val, '#04x')
+
+        # Add formatting so each line stays within 80 characters
+        if (i + 1) < len(hex_data):
+            hex_str += ','
+        if (i + 1) % 12 == 0:
+            hex_str += '\n'
+        hex_array.append(hex_str)
+
+    # Add closing brace
+    c_str += '\n ' + format(' '.join(hex_array)) + '\n};\n\n'
+
+    # Close header guard
+    c_str += '#endif // ' + var_name.upper() + '_H'
+
+    return c_str
+
+
+with open('hotspot_model.h', 'w') as file:
+    file.write(hex_to_c_array(tflite_model, 'hotspot_model'))
