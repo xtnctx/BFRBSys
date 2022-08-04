7bfb673e668515e30a42bf045ca44ae2e6e6f952
Ryan Christopher Bahillo <christopherbahillo@gmail.com>
2022-08-04 17:59:00 +0800

    Add files via upload

--- website/base/models.py
+++ website/base/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class TModels(models.Model):    
+class TrainedModel(models.Model):    
     owner = models.ForeignKey(User, default=None, on_delete=models.CASCADE)
     file = models.FileField(
                             upload_to='TrainedModels', 
