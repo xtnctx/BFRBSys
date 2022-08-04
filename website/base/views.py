dfd6b7a0032621e2d36c6286b27fabd24ecd816f
Ryan Christopher Bahillo <christopherbahillo@gmail.com>
2022-08-04 17:59:43 +0800

    Add files via upload

--- website/base/views.py
+++ website/base/views.py
@@ -1,17 +1,17 @@
-from django.http import HttpResponse
-from django.shortcuts import redirect, render
+from django.http import HttpResponse, JsonResponse
 from django.contrib.auth.decorators import login_required
+from base.utils import hex_to_c_array, rmv_file_spaces
+from django.shortcuts import render
 from django.core.files.base import File
-from base.models import TModels
+from base.models import TrainedModel
+import tensorflow as tf
 import pandas as pd
+import numpy as np
 import csv
 import os
-import tensorflow as tf
-import numpy as np
-from sklearn.model_selection import train_test_split
 
-# Create your views here.
-
+PARAMS = ['ax', 'ay', 'az', 'gx', 'gy', 'gz', 'class']
+SAMPLES_PER_HOTSPOT = 1
 
 def home(request):
     context = {}
@@ -43,68 +43,120 @@ def export_data(request):
         >> classify on and off target soon ... 
         '''
 
-        PARAMS = ['ax', 'ay', 'az', 'gx', 'gy', 'gz', 'class']
         data = request.POST.get('data')
         parsed_csv = list(csv.reader(data.split(';')))
+
         df = pd.DataFrame(parsed_csv, columns=PARAMS)
+        off_target = pd.DataFrame(df[df['class']=='0'].values.tolist(), columns=PARAMS)
+        on_target = pd.DataFrame(df[df['class']=='1'].values.tolist(), columns=PARAMS)
 
-        print(df)
-        return HttpResponse('')
-
-        HOTSPOT = ['true', 'false'] # known location / class
+        HOTSPOT = [off_target, on_target] # known location / class
         NUM_HOTSPOT = len(HOTSPOT)
+        ONE_HOT_ENCODED_HOTSPOT = np.eye(NUM_HOTSPOT)
 
-        df['output'] = [1] * len(df)
-        outputs = df['output']
+        inputs = []
+        outputs = []
 
-        for _ in range(NUM_HOTSPOT):
-            tensor = df.drop(columns='output')
+        for hotspot_index in range(NUM_HOTSPOT):
 
-            for index in range(len(df)):
-                # normalize the input data, between 0 to 1:
-                # - acceleration is between: -4 to +4
-                # - gyroscope is between: -2000 to +2000
+            target = HOTSPOT[hotspot_index]
 
-                tensor['ax'][index] = (float(df['ax'][index]) + 4) / 8
-                tensor['ay'][index] = (float(df['ay'][index]) + 4) / 8
-                tensor['az'][index] = (float(df['az'][index]) + 4) / 8
-                tensor['gx'][index] = (float(df['gx'][index]) + 2000) / 4000
-                tensor['gy'][index] = (float(df['gy'][index]) + 2000) / 4000
-                tensor['gz'][index] = (float(df['gz'][index]) + 2000) / 4000
-                
+            num_recordings = int(target.shape[0] / SAMPLES_PER_HOTSPOT)
 
-        inputs = np.array(tensor).tolist()
-        outputs = np.array(outputs).tolist()
+            output = ONE_HOT_ENCODED_HOTSPOT[hotspot_index]
 
-        X_train, X_test, Y_train, Y_test = train_test_split(inputs, outputs, test_size=0.2)
+            print(f"\tThere are {num_recordings} recordings.")
 
-        # # build the model and train it
+            for i in range(num_recordings):
+                tensor = []
+                for j in range(SAMPLES_PER_HOTSPOT):
+                    index = i * SAMPLES_PER_HOTSPOT + j
+                    # normalize the input data, between 0 to 1:
+                    # - acceleration is between: -4 to +4
+                    # - gyroscope is between: -2000 to +2000
+                    tensor += [
+                        (float(target['ax'][index]) + 4) / 8,
+                        (float(target['ay'][index]) + 4) / 8,
+                        (float(target['az'][index]) + 4) / 8,
+                        (float(target['gx'][index]) + 2000) / 4000,
+                        (float(target['gy'][index]) + 2000) / 4000,
+                        (float(target['gz'][index]) + 2000) / 4000
+                    ]
+                inputs.append(tensor)
+                outputs.append(output)
+
+        # convert the list to numpy array
+        inputs = np.array(inputs)
+        outputs = np.array(outputs)
+
+
+        # Randomize the order of the inputs, so they can be evenly distributed for training, testing, and validation
+        # https://stackoverflow.com/a/37710486/2020087
+        num_inputs = len(inputs)
+        randomize = np.arange(num_inputs)
+        np.random.shuffle(randomize)
+
+        # Swap the consecutive indexes (0, 1, 2, etc) with the randomized indexes
+        inputs = inputs[randomize]
+        outputs = outputs[randomize]
+
+        # Split the recordings (group of samples) into three sets: training, testing and validation
+        TRAIN_SPLIT = int(0.6 * num_inputs)
+        TEST_SPLIT = int(0.2 * num_inputs + TRAIN_SPLIT)
+
+        inputs_train, inputs_test, inputs_validate = np.split(inputs, [TRAIN_SPLIT, TEST_SPLIT])
+        outputs_train, outputs_test, outputs_validate = np.split(outputs, [TRAIN_SPLIT, TEST_SPLIT])
+
+
+        # build the model and train it
         model = tf.keras.Sequential()
-        model.add(tf.keras.layers.Dense(2, activation='relu')) # relu is used for performance
-        model.add(tf.keras.layers.Dense(1, activation='relu'))
+        model.add(tf.keras.layers.Dense(50, activation='relu')) # relu is used for performance
+        model.add(tf.keras.layers.Dense(15, activation='relu'))
         model.add(tf.keras.layers.Dense(NUM_HOTSPOT, activation='softmax')) # softmax is used, because we only expect one hotspot to occur per input
         model.compile(optimizer='rmsprop', loss='mse', metrics=['mae'])
-        history = model.fit(X_train, Y_train, epochs=100)
+        history = model.fit(inputs_train, outputs_train, epochs=600, batch_size=1, 
+                            validation_data=(inputs_validate, outputs_validate))
 
-        print("Evaluate on test data")
-        results = model.evaluate(X_test, Y_test)
-        print("test loss, test acc:", results)
-        
-        # use the model to predict the test inputs
-        feed = [[1,1,1,1,1,1]]
-        predictions = model.predict(feed)
 
+        # print("Evaluate on test data")
+        # results = model.evaluate(inputs_test, outputs_test)
+        # print("test loss, test acc:", results)
+
+
+        predictions = model.predict(inputs_test)
         # print the predictions and the expected ouputs
         print("predictions =\n", np.round(predictions, decimals=3))
-        # print("actual =\n", Y_test)
+        print("actual =\n", outputs_test)
 
-        # df.to_csv('static/temp.csv', encoding='utf-8', index=False)
+        # # use the model to predict the test inputs
+        # feed = [[1,1,1,1,1,1]]
+        # predictions = model.predict(feed)
 
-        # with open('static/temp.csv', 'rb') as f:
-        #     fs = TModels(owner=request.user, file=File(f, name=os.path.basename(f.name)))
-        #     fs.save()
+        # # print the predictions and the expected ouputs
+        # print("predictions =\n", np.round(predictions, decimals=3))
+
+
+        # Convert the model to the TensorFlow Lite format without quantization
+        converter = tf.lite.TFLiteConverter.from_keras_model(model)
+        converter.optimizations = [tf.lite.Optimize.OPTIMIZE_FOR_SIZE]
+        tflite_model = converter.convert()
         
-        # os.remove('static/temp.csv')
+
+        # Save as temporary
+        with open('static/temp_model.h', 'w') as temp:
+            temp.write(hex_to_c_array(tflite_model))
+
+        # Then save to database
+        with open('static/temp_model.h', 'rb') as f:
+            fs = TrainedModel(owner=request.user, file=File(f, name=os.path.basename(f.name)))
+            fs.save()
+        
+        model_string = rmv_file_spaces('static/temp_model.h', exclude='unsigned char model[] = {')
+
+        os.remove('static/temp_model.h') # end of using temp model
+
+        context = {'model_string': model_string}
+        return JsonResponse(context)
     
     return HttpResponse('')
 
