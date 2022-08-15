5f1db0d71e6bffd7cca6b8e1754d6f0bd331c93b
Ryan Christopher Bahillo <christopherbahillo@gmail.com>
2022-08-15 17:54:45 +0800

    Add files via upload

--- website/base/urls.py
+++ website/base/urls.py
@@ -5,6 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('app/', views.app, name='app'),
-    path('graph/', views.graph, name='graph'),
-    path('graph/export/', views.export_data, name='export'), # successor of url: graph/
+    path('app/export/', views.export_data, name='export'), # successor of url: app/
 ]
