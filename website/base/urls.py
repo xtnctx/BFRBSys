ffd52b2fe896d2413a51f55f56bd185e32c094b3
Ryan Christopher Bahillo <christopherbahillo@gmail.com>
2022-08-17 20:03:31 +0800

    stats url

--- website/base/urls.py
+++ website/base/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('app/', views.app, name='app'),
-    path('app/export/', views.export_data, name='export'), # successor of url: app/
+    path('app/export/', views.train_model, name='export'), # successor of url: app/
+    path('app/status/', views.get_status, name='status'), # successor of url: app/
 ]
