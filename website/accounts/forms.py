120b8c54b653630f433e4e87e21c9bcf34cac422
Ryan Christopher Bahillo <christopherbahillo@gmail.com>
2022-08-25 20:37:17 +0800

    firstname and lastname added

--- website/accounts/forms.py
+++ website/accounts/forms.py
@@ -4,8 +4,10 @@ from django import forms
 
 
 class RegisterForm(UserCreationForm):
+    first_name = forms.CharField(max_length=50)
+    last_name = forms.CharField(max_length=50)
     email = forms.EmailField()
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
