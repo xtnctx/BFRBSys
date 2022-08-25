e5ee6006693b6c17293d56881a7f8c966c67a3d6
Ryan Christopher Bahillo <christopherbahillo@gmail.com>
2022-08-25 20:40:50 +0800

    account urls

--- website/accounts/urls.py
+++ website/accounts/urls.py
@@ -6,6 +6,9 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('register/', views.register_view, name='register'),
+
     path('account_info/', views.account_info, name='account_info'),
+    path('account_info/edit_account/', views.edit_account, name='edit_account'),
+    path('account_info/edit_account/change_password/', views.UpdatePassword.as_view(), name='change_password'),
 
 ]
