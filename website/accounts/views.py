bfeed10787caf03c3f0528d3a750e19b92b1ed83
Ryan Christopher Bahillo <christopherbahillo@gmail.com>
2022-08-25 20:41:24 +0800

    update user profile and password views

--- website/accounts/views.py
+++ website/accounts/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from accounts.forms import RegisterForm
 from base.models import TrainingStatus
+from django.contrib.auth.views import PasswordChangeView
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth.models import User
+from django.contrib import messages
+from validate_email import validate_email
 
 # Create your views here.
 
@@ -25,7 +30,7 @@ def login_view(request):
     # return redirect(request.POST.get('next','../app/'))
 
 def logout_view(request):
-    if request.method == 'POST':
+    if request.method == 'POST': 
         logout(request)
         return redirect('home')
     context = {}
@@ -51,7 +56,47 @@ def register_view(request):
     context = {'form': form}
     return render(request, 'accounts/register.html', context)
 
-
 def account_info(request):
+    if request.method == 'POST':
+        ...
     context = {}
     return render(request, 'accounts/account_info.html', context)
+
+def edit_account(request):
+    if request.method == 'POST':
+        newusername = " ".join(request.POST.get('newusername').split())
+        newemail = " ".join(request.POST.get('newemail').split())
+        newfirstname = " ".join(request.POST.get('newfirstname').split())
+        newlastname = " ".join(request.POST.get('newlastname').split())
+
+        user = User.objects.get(username = request.user.username)
+        if newusername != '':
+            user.username = newusername
+            user.save()
+        
+        if newfirstname != '':
+            user.first_name = newfirstname.title()
+            user.save()
+
+        if newlastname != '':
+            user.last_name = newlastname.title()
+            user.save()
+        
+        if validate_email(newemail):
+            user.email = newemail
+            user.save()
+        elif newemail != '':
+            messages.error(request, 'Invalid email')
+    
+        print(request.user.first_name)
+        return redirect('edit_account')
+
+    context = {}
+    return render(request, 'accounts/edit_account.html', context)
+
+
+
+class UpdatePassword(PasswordChangeView):
+    form_class = PasswordChangeForm
+    success_url = '/edit_account'
+    template_name = 'accounts/changepassword.html'
