b35d965c28b26a052123ca1f4035a03beae422ef
Ryan Christopher Bahillo <christopherbahillo@gmail.com>
2022-08-27 21:55:48 +0800

    Add files via upload

--- website/accounts/static/accounts/scripts/profile_pic.js
+++ website/accounts/static/accounts/scripts/profile_pic.js
@@ -1,9 +1,16 @@
+document.querySelector("html").classList.add('js');
+
 //declearing html elements
 
-const imgDiv = document.querySelector('.profile-pic-div');
+const imgDiv = document.querySelector('.image-container');
 const img = document.querySelector('#photo');
-const file = document.querySelector('#file');
+const file = document.querySelector('.input-file');
 const uploadBtn = document.querySelector('#uploadBtn');
+
+// init our variables
+var fileInput = document.querySelector( ".input-file" ),
+	button = document.querySelector( ".input-file-trigger" );
+
 
 //if user hover on img div 
 
@@ -33,4 +40,18 @@ file.addEventListener('change', function(){
 
         reader.readAsDataURL(choosedFile);
     }
+});
+
+
+// Trigger when Space bar or Enter is hit
+button.addEventListener( "keydown", function( event ) {
+	if ( event.keyCode == 13 || event.keyCode == 32 ) {
+		fileInput.focus();
+	}
+});
+
+// Trigger when the label is clicked
+button.addEventListener( "click", function( event ) {
+	fileInput.focus();
+	return false;
 });
