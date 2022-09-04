e03357c7c7a2d5515b4285ecd56dba99744de3f2
Ryan Christopher Bahillo <christopherbahillo@gmail.com>
2022-09-04 20:29:12 +0800

    fix admin and accounts

--- website/accounts/admin.py
+++ website/accounts/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from . models import Profile
+from django.db.models.signals import pre_delete
+from django.dispatch.dispatcher import receiver
 
 admin.site.register(Profile)
+
+@receiver(pre_delete, sender=Profile)
+def onProfileDelete(sender, instance, **kwargs):
+    # Pass false so ImageField doesn't save the model.
+    if instance.image.name != 'default.jpg':
+        instance.image.delete(False)
