7a002c2412c9ced67f26f00288edc002e8a22bf7
Ryan Christopher Bahillo <christopherbahillo@gmail.com>
2022-10-16 18:39:17 +0800

    Add files via upload

---
+++ FlutterApp/lib/profile_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+int itemCount = 20;
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: itemCount,
+      itemBuilder: (BuildContext context, int index) {
+        return ListTile(
+          title: Text('Item ${index + 1}'),
+          leading: const Icon(Icons.person),
+          trailing: const Icon(Icons.flash_on),
+          onTap: () {
+            debugPrint('Item ${index + 1} selected');
+          },
+        );
+      },
+    );
+  }
+}
