df7e631349ea6094305366e1c4f9527565797d0e
Ryan Christopher Bahillo <christopherbahillo@gmail.com>
2022-10-21 20:27:04 +0800

    Add files via upload

---
+++ FlutterApp/lib/results_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ResultsPage extends StatelessWidget {
+  const ResultsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
