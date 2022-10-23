9317357a2dd04c3cd9fe588633bb1a7828146eff
Ryan Christopher Bahillo <christopherbahillo@gmail.com>
2022-10-23 19:56:10 +0800

    Add files via upload

--- FlutterApp/lib/results_page.dart
+++ FlutterApp/lib/results_page.dart
@@ -1,10 +1,44 @@
 import 'package:flutter/material.dart';
 
-class ResultsPage extends StatelessWidget {
+class ResultsPage extends StatefulWidget {
   const ResultsPage({super.key});
 
   @override
+  State<ResultsPage> createState() => _ResultsPageState();
+}
+
+class _ResultsPageState extends State<ResultsPage> {
+  int _index = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: SizedBox(
+          height: 200, // card height
+          child: PageView.builder(
+            itemCount: 3,
+            controller: PageController(viewportFraction: 0.7),
+            onPageChanged: (int index) => setState(() => _index = index),
+            itemBuilder: (_, i) {
+              return Transform.scale(
+                scale: i == _index ? 1 : 0.9,
+                child: Card(
+                  elevation: 6,
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                  child: Center(
+                    child: Text(
+                      "Card ${i + 1}",
+                      style: TextStyle(fontSize: 32),
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ),
+    );
   }
 }
