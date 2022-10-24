7b2c31079df2c1509b9954d85803cc02048d181d
Ryan Christopher Bahillo <christopherbahillo@gmail.com>
2022-10-24 23:24:56 +0800

    Add files via upload

--- FlutterApp/lib/home_page.dart
+++ FlutterApp/lib/home_page.dart
@@ -1,23 +1,27 @@
 import 'package:bfrbsys/ble_connection.dart';
 import 'package:flutter/material.dart';
 
-class HomePapge extends StatelessWidget {
-  const HomePapge({super.key});
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+  final Icon navBarIcon = const Icon(Icons.home_outlined);
+  final Icon navBarIconSelected = const Icon(Icons.home);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: ElevatedButton(
-        onPressed: () {
-          Navigator.of(context).push(
-            MaterialPageRoute(
-              builder: (BuildContext context) {
-                return const BluetoothBuilderPage();
-              },
-            ),
-          );
-        },
-        child: const Text('Connect'),
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (BuildContext context) {
+                  return const BluetoothBuilderPage();
+                },
+              ),
+            );
+          },
+          child: const Text('Start'),
+        ),
       ),
     );
   }
