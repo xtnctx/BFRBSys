fd55024f6e94c06007882b59e4fbd2b5ae329c20
Ryan Christopher Bahillo <christopherbahillo@gmail.com>
2022-10-30 20:10:28 +0800

    Add files via upload

--- FlutterApp/lib/profile_page.dart
+++ FlutterApp/lib/profile_page.dart
@@ -1,26 +1,34 @@
 import 'package:flutter/material.dart';
 
-int itemCount = 20;
-
-class ProfilePage extends StatelessWidget {
-  const ProfilePage({super.key});
+class ProfilePage extends StatefulWidget {
   final Icon navBarIcon = const Icon(Icons.person_outlined);
   final Icon navBarIconSelected = const Icon(Icons.person);
+  final String navBarTitle = 'Profile';
+  const ProfilePage({super.key});
+
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  int itemCount = 20;
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: itemCount,
-      itemBuilder: (BuildContext context, int index) {
-        return ListTile(
-          title: Text('Item ${index + 1}'),
-          leading: const Icon(Icons.person),
-          trailing: const Icon(Icons.flash_on),
-          onTap: () {
-            debugPrint('Item ${index + 1} selected');
-          },
-        );
-      },
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: itemCount,
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            title: Text('Item ${index + 1}'),
+            leading: const Icon(Icons.person),
+            trailing: const Icon(Icons.flash_on),
+            onTap: () {
+              debugPrint('Item ${index + 1} selected');
+            },
+          );
+        },
+      ),
     );
   }
 }
