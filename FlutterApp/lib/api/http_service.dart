79808a96da13acb280037b2ac51b000cde7ad5c1
Ryan Christopher Bahillo <christopherbahillo@gmail.com>
2022-11-07 20:13:42 +0800

    Add files via upload

--- FlutterApp/lib/api/http_service.dart
+++ FlutterApp/lib/api/http_service.dart
@@ -1,5 +1,5 @@
 import 'package:bfrbsys/api/env.dart';
-import 'package:bfrbsys/api/model/item.dart';
+import 'package:bfrbsys/api/models/models.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
@@ -13,5 +13,27 @@ class HttpService {
     }).toList();
 
     return employees;
+  }
+
+  Future<TrainedModels> post(String modelName) async {
+    final response = await http.post(
+      Uri.parse("${Env.URL_PREFIX}/api/"),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'model_name': modelName,
+      }),
+    );
+
+    if (response.statusCode == 201) {
+      // If the server did return a 201 CREATED response,
+      // then parse the JSON.
+      return TrainedModels.fromJson(jsonDecode(response.body));
+    } else {
+      // If the server did not return a 201 CREATED response,
+      // then throw an exception.
+      throw Exception('Failed to create model.');
+    }
   }
 }
