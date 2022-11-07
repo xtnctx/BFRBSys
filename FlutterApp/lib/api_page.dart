31197e5e44daa4ca09920812f532fe6e390ca448
Ryan Christopher Bahillo <christopherbahillo@gmail.com>
2022-11-07 20:14:09 +0800

    Add files via upload

--- FlutterApp/lib/api_page.dart
+++ FlutterApp/lib/api_page.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 import 'package:bfrbsys/api/http_service.dart';
-import 'package:bfrbsys/api/model/item.dart';
+import 'package:bfrbsys/api/models/models.dart';
 import 'package:flutter/material.dart';
 
 class ApiService extends StatefulWidget {
@@ -18,6 +18,9 @@ class _ApiServiceState extends State<ApiService> {
   HttpService httpService = HttpService();
   Future<List<Item>>? _items;
 
+  final TextEditingController _controller = TextEditingController();
+  Future<TrainedModels>? _futureModels;
+
   @override
   void initState() {
     super.initState();
@@ -26,44 +29,89 @@ class _ApiServiceState extends State<ApiService> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: FutureBuilder<List<Item>>(
-          future: _items,
-          builder: (BuildContext context, AsyncSnapshot<List<Item>> snapshot) {
-            // By default, show a loading spinner.
-            if (!snapshot.hasData) return const CircularProgressIndicator();
+    return Scaffold(body: postWidget());
+  }
 
-            // if (snapshot.hasError) return Text('Error: ${snapshot.error}');
-            // switch (snapshot.connectionState) {
-            //   case ConnectionState.none:
-            //     return Text('Select lot');
-            //   case ConnectionState.waiting:
-            //     return Text('Awaiting bids...');
-            //   case ConnectionState.active:
-            //     return Text('\$${snapshot.data}');
-            //   case ConnectionState.done:
-            //     return Text('\$${snapshot.data} (closed)');
-            // }
-            // Render employee lists
-            return ListView.builder(
-              itemCount: snapshot.data?.length,
-              itemBuilder: (BuildContext context, int index) {
-                var data = snapshot.data![index];
-                return Card(
-                  child: ListTile(
-                    leading: const Icon(Icons.person),
-                    title: Text(
-                      data.name,
-                      style: const TextStyle(fontSize: 20),
-                    ),
+  Center getWidget() {
+    return Center(
+      child: FutureBuilder<List<Item>>(
+        future: _items,
+        builder: (BuildContext context, AsyncSnapshot<List<Item>> snapshot) {
+          // By default, show a loading spinner.
+          if (!snapshot.hasData) return const CircularProgressIndicator();
+
+          // if (snapshot.hasError) return Text('Error: ${snapshot.error}');
+          // switch (snapshot.connectionState) {
+          //   case ConnectionState.none:
+          //     return Text('Select lot');
+          //   case ConnectionState.waiting:
+          //     return Text('Awaiting bids...');
+          //   case ConnectionState.active:
+          //     return Text('\$${snapshot.data}');
+          //   case ConnectionState.done:
+          //     return Text('\$${snapshot.data} (closed)');
+          // }
+          // Render employee lists
+          return ListView.builder(
+            itemCount: snapshot.data?.length,
+            itemBuilder: (BuildContext context, int index) {
+              var data = snapshot.data![index];
+              return Card(
+                child: ListTile(
+                  leading: const Icon(Icons.person),
+                  title: Text(
+                    data.name,
+                    style: const TextStyle(fontSize: 20),
                   ),
-                );
-              },
-            );
-          },
-        ),
+                ),
+              );
+            },
+          );
+        },
       ),
+    );
+  }
+
+  Container postWidget() {
+    return Container(
+      alignment: Alignment.center,
+      padding: const EdgeInsets.all(8.0),
+      child: (_futureModels == null) ? buildColumn() : buildFutureBuilder(),
+    );
+  }
+
+  Column buildColumn() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        TextField(
+          controller: _controller,
+          decoration: const InputDecoration(hintText: 'Enter Title'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            setState(() {
+              _futureModels = httpService.post(_controller.text);
+            });
+          },
+          child: const Text('Create Data'),
+        ),
+      ],
+    );
+  }
+
+  FutureBuilder<TrainedModels> buildFutureBuilder() {
+    return FutureBuilder<TrainedModels>(
+      future: _futureModels,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return Text(snapshot.data!.modelName);
+        } else if (snapshot.hasError) {
+          return Text('${snapshot.error}');
+        }
+
+        return const CircularProgressIndicator();
+      },
     );
   }
 }
