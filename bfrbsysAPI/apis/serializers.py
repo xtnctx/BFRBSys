64ffa52e2cd6bafc6df979d9d666ed8676468952
Ryan Christopher Bahillo <christopherbahillo@gmail.com>
2022-11-08 20:54:26 +0800

    Add files via upload

--- bfrbsysAPI/apis/serializers.py
+++ bfrbsysAPI/apis/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from .models import Item, TrainedModel
+from django.contrib.auth.models import User
+from rest_framework.validators import UniqueTogetherValidator
 
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,3 +12,26 @@ class TrainedModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = TrainedModel
         fields = '__all__'
+
+
+
+class UserSerializer(serializers.ModelSerializer):
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            'first_name',
+            'last_name',
+            'email',
+            'password',
+        )
+        validators = [
+            UniqueTogetherValidator(
+                queryset=User.objects.all(),
+                fields=['username', 'email']
+            )
+        ]
