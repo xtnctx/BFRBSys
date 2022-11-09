7c85cdc0d98ff0999b0b5f2df950f9db0b9ab6b7
Ryan Christopher Bahillo <christopherbahillo@gmail.com>
2022-11-09 21:46:48 +0800

    Add files via upload

--- bfrbsysAPI/apis/urls.py
+++ bfrbsysAPI/apis/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from knox import views as knox_views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('api/', views.NeuralNetworkBuilderApi.as_view(), name='api'),
-    
+    # path('api/', views.NeuralNetworkBuilder.as_view(), name='api'),
+    # path('user/', views.UserCreation.as_view(), name='users'),
+    path('api/auth/register/', views.RegisterAPI.as_view()),
+    path('api/auth/login/', views.LoginAPI.as_view()),
+    path('api/auth/user/', views.UserAPI.as_view()),
+    path('api/auth/logout/', knox_views.LogoutView.as_view(), name='knox_logout'),
+    path('api/auth/', include('knox.urls')),
 ]
