1e0cc4ea6a93594e9206fd9995d7c2fb8158be7b
Ryan Christopher Bahillo <christopherbahillo@gmail.com>
2022-11-11 23:22:04 +0800

    Add files via upload

---
+++ FlutterApp/lib/register_page.dart
@@ -0,0 +1,259 @@
+import 'dart:convert';
+
+import 'package:bfrbsys/api/http_service.dart';
+import 'package:bfrbsys/api/models/models.dart';
+import 'package:bfrbsys/user_secure_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final userNameController = TextEditingController();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final confirmPasswordController = TextEditingController();
+
+  final storage = const FlutterSecureStorage();
+  bool _obscureText = true;
+
+  HttpService httpService = HttpService();
+  Future<RegisterModel>? _futureRegister;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Center(
+          child: ListView(
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            // mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Hello Again
+              Center(
+                child: Text(
+                  'Sign Up',
+                  style: GoogleFonts.bebasNeue(fontSize: 50),
+                ),
+              ),
+              const SizedBox(height: 10),
+              const Center(
+                child: Text(
+                  'Create an account, ready your wearable.',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+              const SizedBox(height: 50),
+
+              // Username textfield
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    border: Border.all(color: Colors.white),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: TextField(
+                    controller: userNameController,
+                    autocorrect: false,
+                    toolbarOptions: const ToolbarOptions(
+                      copy: true,
+                      cut: true,
+                      paste: true,
+                      selectAll: true,
+                    ),
+                    decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      focusedBorder: InputBorder.none,
+                      enabledBorder: InputBorder.none,
+                      errorBorder: InputBorder.none,
+                      disabledBorder: InputBorder.none,
+                      hintText: 'Username',
+                      contentPadding: EdgeInsets.only(left: 20.0),
+                    ),
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 10),
+
+              // Email textfield
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    border: Border.all(color: Colors.white),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: TextField(
+                    controller: emailController,
+                    keyboardType: TextInputType.emailAddress,
+                    autocorrect: false,
+                    toolbarOptions: const ToolbarOptions(
+                      copy: true,
+                      cut: true,
+                      paste: true,
+                      selectAll: true,
+                    ),
+                    decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      focusedBorder: InputBorder.none,
+                      enabledBorder: InputBorder.none,
+                      errorBorder: InputBorder.none,
+                      disabledBorder: InputBorder.none,
+                      hintText: 'Email',
+                      contentPadding: EdgeInsets.only(left: 20.0),
+                    ),
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 10),
+
+              // Password textfield
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    border: Border.all(color: Colors.white),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: TextField(
+                          controller: passwordController,
+                          obscureText: _obscureText,
+                          autocorrect: false,
+                          enableSuggestions: false,
+                          textAlignVertical: TextAlignVertical.center,
+                          toolbarOptions: const ToolbarOptions(),
+                          decoration: InputDecoration(
+                            suffixIcon: IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  _obscureText = !_obscureText;
+                                });
+                              },
+                              icon: Icon(_obscureText ? Icons.visibility : Icons.visibility_off),
+                            ),
+                            border: InputBorder.none,
+                            focusedBorder: InputBorder.none,
+                            enabledBorder: InputBorder.none,
+                            errorBorder: InputBorder.none,
+                            disabledBorder: InputBorder.none,
+                            hintText: 'Password',
+                            contentPadding: const EdgeInsets.only(left: 20.0),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 10),
+
+              // Confirm password textfield
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    border: Border.all(color: Colors.white),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: TextField(
+                    controller: confirmPasswordController,
+                    autocorrect: false,
+                    enableSuggestions: false,
+                    obscureText: true,
+                    toolbarOptions: const ToolbarOptions(),
+                    decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      focusedBorder: InputBorder.none,
+                      enabledBorder: InputBorder.none,
+                      errorBorder: InputBorder.none,
+                      disabledBorder: InputBorder.none,
+                      hintText: 'Confirm password',
+                      contentPadding: EdgeInsets.only(left: 20.0),
+                    ),
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 25),
+
+              // Sign in button
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: ConstrainedBox(
+                  constraints: const BoxConstraints.tightFor(height: 70),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Send post request to server
+                      setState(() {
+                        _futureRegister = httpService.postRegister(
+                          username: userNameController.text,
+                          email: emailController.text,
+                          password: passwordController.text,
+                        );
+                      });
+
+                      // Save response (user credentials) to secure storage
+                      _futureRegister?.then((value) async {
+                        await UserSecureStorage.setUser(user: value.user);
+                        await UserSecureStorage.setToken(token: value.token);
+                      });
+                    },
+                    child: const Text(
+                      'Sign Up',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 25),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Already have an account?',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text(
+                      ' Sign In',
+                      style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
