da0e5c3355ed4e27d4a7a4f5649785b98c0edba2
Ryan Christopher Bahillo <christopherbahillo@gmail.com>
2022-11-11 23:21:42 +0800

    Add files via upload

--- FlutterApp/lib/login_page.dart
+++ FlutterApp/lib/login_page.dart
@@ -1,3 +1,5 @@
+import 'package:bfrbsys/register_page.dart';
+import 'package:bfrbsys/user_secure_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -9,6 +11,11 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  bool _obscureText = true;
+
+  // late Map<String, dynamic> user;
+  // late String token;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,30 +25,25 @@ class _LoginPageState extends State<LoginPage> {
           child: ListView(
             shrinkWrap: true,
             physics: const NeverScrollableScrollPhysics(),
-            // mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Icon(
-                Icons.android,
-                size: 100,
-              ),
-              const SizedBox(height: 50),
               // Hello Again
               Center(
                 child: Text(
-                  'Hello Again!',
-                  style: GoogleFonts.bebasNeue(fontSize: 52),
+                  'Sign In',
+                  style: GoogleFonts.bebasNeue(fontSize: 50),
                 ),
               ),
               const SizedBox(height: 10),
               const Center(
                 child: Text(
                   'Welcome back, you\'ve been missed!',
-                  style: TextStyle(fontSize: 20),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 18),
                 ),
               ),
               const SizedBox(height: 50),
 
-              // Email textfield
+              // Username textfield
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 25.0),
                 child: Container(
@@ -51,13 +53,20 @@ class _LoginPageState extends State<LoginPage> {
                     borderRadius: BorderRadius.circular(12),
                   ),
                   child: const TextField(
+                    autocorrect: false,
+                    toolbarOptions: ToolbarOptions(
+                      copy: true,
+                      cut: true,
+                      paste: true,
+                      selectAll: true,
+                    ),
                     decoration: InputDecoration(
                       border: InputBorder.none,
                       focusedBorder: InputBorder.none,
                       enabledBorder: InputBorder.none,
                       errorBorder: InputBorder.none,
                       disabledBorder: InputBorder.none,
-                      hintText: 'Email',
+                      hintText: 'Username',
                       contentPadding: EdgeInsets.only(left: 20.0),
                     ),
                   ),
@@ -75,34 +84,59 @@ class _LoginPageState extends State<LoginPage> {
                     border: Border.all(color: Colors.white),
                     borderRadius: BorderRadius.circular(12),
                   ),
-                  child: const TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      border: InputBorder.none,
-                      focusedBorder: InputBorder.none,
-                      enabledBorder: InputBorder.none,
-                      errorBorder: InputBorder.none,
-                      disabledBorder: InputBorder.none,
-                      hintText: 'Password',
-                      contentPadding: EdgeInsets.only(left: 20.0),
-                    ),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: TextField(
+                          obscureText: _obscureText,
+                          autocorrect: false,
+                          enableSuggestions: false,
+                          textAlignVertical: TextAlignVertical.center,
+                          toolbarOptions: const ToolbarOptions(
+                            copy: true,
+                            cut: true,
+                            paste: true,
+                            selectAll: true,
+                          ),
+                          decoration: InputDecoration(
+                            suffixIcon: IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  _obscureText = !_obscureText;
+                                });
+                              },
+                              icon: Icon(_obscureText ? Icons.visibility : Icons.visibility_off),
+                            ),
+                            border: InputBorder.none,
+                            focusedBorder: InputBorder.none,
+                            enabledBorder: InputBorder.none,
+                            errorBorder: InputBorder.none,
+                            disabledBorder: InputBorder.none,
+                            hintText: 'Password',
+                            contentPadding: const EdgeInsets.only(left: 20.0),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ),
 
-              const SizedBox(height: 10),
+              const SizedBox(height: 25),
 
               // Sign in button
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Container(
-                  padding: const EdgeInsets.all(20.0),
-                  decoration: BoxDecoration(
-                    color: Colors.deepPurple,
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: const Center(
-                    child: Text(
+                child: ConstrainedBox(
+                  constraints: const BoxConstraints.tightFor(height: 70),
+                  child: ElevatedButton(
+                    onPressed: () async {
+                      String user = await UserSecureStorage.getUser();
+                      String token = await UserSecureStorage.getToken();
+
+                      print('$user $token');
+                    },
+                    child: const Text(
                       'Sign In',
                       style: TextStyle(
                         color: Colors.white,
@@ -118,14 +152,22 @@ class _LoginPageState extends State<LoginPage> {
 
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  Text(
-                    'Not a member?',
+                children: [
+                  const Text(
+                    'Don\'t have an account?',
                     style: TextStyle(fontWeight: FontWeight.bold),
                   ),
-                  Text(
-                    ' Register now',
-                    style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const RegisterPage()),
+                      );
+                    },
+                    child: const Text(
+                      ' Sign Up',
+                      style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),
+                    ),
                   ),
                 ],
               )
