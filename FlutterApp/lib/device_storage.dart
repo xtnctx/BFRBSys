9623a7f936ea2156e90df4dc4a374d58b7544a3e
Ryan Christopher Bahillo <christopherbahillo@gmail.com>
2022-11-19 15:45:27 +0800

    Add files via upload

--- FlutterApp/lib/device_storage.dart
+++ FlutterApp/lib/device_storage.dart
@@ -11,8 +11,11 @@ class UserSecureStorage {
 
   static Future<Map<String, dynamic>> getUser() async {
     var value = await _storage.read(key: _keyUser);
-    Map<String, dynamic> user = json.decode(value!);
-    return user;
+    if (value != null) {
+      Map<String, dynamic> user = json.decode(value);
+      return user;
+    }
+    return {};
   }
 
   static Future setUser({required Map<String, dynamic> user}) async {
@@ -22,8 +25,11 @@ class UserSecureStorage {
 
   static Future<String> getToken() async {
     var value = await _storage.read(key: _keyToken);
-    String token = json.decode(value!);
-    return token;
+    if (value != null) {
+      String token = json.decode(value);
+      return token;
+    }
+    return '';
   }
 
   static Future setToken({required String token}) async {
