b29937164e368a1b3dc7787004eb0bcbe03956b2
Ryan Christopher Bahillo <christopherbahillo@gmail.com>
2022-11-20 21:19:18 +0800

    Add files via upload

--- FlutterApp/lib/api/env.dart
+++ FlutterApp/lib/api/env.dart
@@ -1,5 +1,5 @@
 // ignore_for_file: non_constant_identifier_names
 
 class Env {
-  static String URL_PREFIX = "http://spyxanya.pythonanywhere.com/";
+  static String URL_PREFIX = "http://10.0.2.2:8000";
 }
