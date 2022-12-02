43bdc0a1be340a767d9679dec11f338260eb85e7
Ryan Christopher Bahillo <christopherbahillo@gmail.com>
2022-12-02 17:09:38 +0800

    Add files via upload

--- bfrbsysAPI/apis/utils.py
+++ bfrbsysAPI/apis/utils.py
@@ -1,25 +1,24 @@
 import re, os
+import pandas as pd
 
 def hex_to_c_array(hex_data) -> str:
     c_str = ''
 
     # Declare C variable
-    c_str += 'unsigned char model[] = {'
+    c_str += 'unsigned char model[]={'
     hex_array = []
 
     for i, val in enumerate(hex_data):
         # Construct string from hex
         hex_str = format(val, '#04x')
 
-        # Add formatting so each line stays within 80 characters
         if (i + 1) < len(hex_data):
             hex_str += ','
-        if (i + 1) % 12 == 0:
-            hex_str += '\n'
+
         hex_array.append(hex_str)
 
     # Add closing brace
-    c_str += '\n ' + format(' '.join(hex_array)) + '\n};'
+    c_str += format(''.join(hex_array)) + '};'
 
     return c_str
 
@@ -49,3 +48,9 @@ def remove_file(path: str):
         os.remove(path)
     except PermissionError as e:
         print(e)
+
+def callback_string(history) -> str:
+    hist_df = pd.DataFrame(history.history)
+    str_callback = hist_df.to_csv(index=False)
+    return str_callback
+    
