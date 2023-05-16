2220e93b1331c762ce56d09e6945563520fb0826
Ryan Christopher Bahillo <christopherbahillo@gmail.com>
2023-05-16 18:40:53 +0800

    added options

--- FlutterApp/lib/api/env.dart
+++ FlutterApp/lib/api/env.dart
@@ -1,6 +1,12 @@
 // ignore_for_file: non_constant_identifier_names
 
 class Env {
-  static String BASE_URL = "http://10.0.2.2:8000";
+  // Physical device
+  static String BASE_URL = "http://192.168.1.1:8000";
+
+  // Emulator
+  // static String BASE_URL = "http://10.0.2.2:8000";
+
+  // Hosted
   // static String BASE_URL = "http://spyxanya.pythonanywhere.com/";
 }
