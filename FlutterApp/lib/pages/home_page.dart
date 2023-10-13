c93ff7644bb2191947d58c41da2fc1107239a3df
Ryan Christopher Bahillo <christopherbahillo@gmail.com>
2023-10-14 00:57:22 +0800

    Add files via upload

--- FlutterApp/lib/pages/home_page.dart
+++ FlutterApp/lib/pages/home_page.dart
@@ -11,7 +11,210 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+  late AnimationController firstRippleController;
+  late AnimationController secondRippleController;
+  late AnimationController thirdRippleController;
+  late AnimationController centerCircleController;
+  late Animation<double> firstRippleRadiusAnimation;
+  late Animation<double> firstRippleOpacityAnimation;
+  late Animation<double> firstRippleWidthAnimation;
+  late Animation<double> secondRippleRadiusAnimation;
+  late Animation<double> secondRippleOpacityAnimation;
+  late Animation<double> secondRippleWidthAnimation;
+  late Animation<double> thirdRippleRadiusAnimation;
+  late Animation<double> thirdRippleOpacityAnimation;
+  late Animation<double> thirdRippleWidthAnimation;
+  late Animation<double> centerCircleRadiusAnimation;
+
+  @override
+  void initState() {
+    firstRippleController = AnimationController(
+      vsync: this,
+      duration: const Duration(
+        seconds: 2,
+      ),
+    );
+
+    firstRippleRadiusAnimation = Tween<double>(begin: 0, end: 150).animate(
+      CurvedAnimation(
+        parent: firstRippleController,
+        curve: Curves.ease,
+      ),
+    )
+      ..addListener(() {
+        setState(() {});
+      })
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          firstRippleController.repeat();
+        } else if (status == AnimationStatus.dismissed) {
+          firstRippleController.forward();
+        }
+      });
+
+    firstRippleOpacityAnimation = Tween<double>(begin: 1, end: 0).animate(
+      CurvedAnimation(
+        parent: firstRippleController,
+        curve: Curves.ease,
+      ),
+    )..addListener(
+        () {
+          setState(() {});
+        },
+      );
+
+    firstRippleWidthAnimation = Tween<double>(begin: 10, end: 0).animate(
+      CurvedAnimation(
+        parent: firstRippleController,
+        curve: Curves.ease,
+      ),
+    )..addListener(
+        () {
+          setState(() {});
+        },
+      );
+
+    secondRippleController = AnimationController(
+      vsync: this,
+      duration: const Duration(
+        seconds: 2,
+      ),
+    );
+
+    secondRippleRadiusAnimation = Tween<double>(begin: 0, end: 150).animate(
+      CurvedAnimation(
+        parent: secondRippleController,
+        curve: Curves.ease,
+      ),
+    )
+      ..addListener(() {
+        setState(() {});
+      })
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          secondRippleController.repeat();
+        } else if (status == AnimationStatus.dismissed) {
+          secondRippleController.forward();
+        }
+      });
+
+    secondRippleOpacityAnimation = Tween<double>(begin: 1, end: 0).animate(
+      CurvedAnimation(
+        parent: secondRippleController,
+        curve: Curves.ease,
+      ),
+    )..addListener(
+        () {
+          setState(() {});
+        },
+      );
+
+    secondRippleWidthAnimation = Tween<double>(begin: 10, end: 0).animate(
+      CurvedAnimation(
+        parent: secondRippleController,
+        curve: Curves.ease,
+      ),
+    )..addListener(
+        () {
+          setState(() {});
+        },
+      );
+
+    thirdRippleController = AnimationController(
+      vsync: this,
+      duration: const Duration(
+        seconds: 2,
+      ),
+    );
+
+    thirdRippleRadiusAnimation = Tween<double>(begin: 0, end: 150).animate(
+      CurvedAnimation(
+        parent: thirdRippleController,
+        curve: Curves.ease,
+      ),
+    )
+      ..addListener(() {
+        setState(() {});
+      })
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          thirdRippleController.repeat();
+        } else if (status == AnimationStatus.dismissed) {
+          thirdRippleController.forward();
+        }
+      });
+
+    thirdRippleOpacityAnimation = Tween<double>(begin: 1, end: 0).animate(
+      CurvedAnimation(
+        parent: thirdRippleController,
+        curve: Curves.ease,
+      ),
+    )..addListener(
+        () {
+          setState(() {});
+        },
+      );
+
+    thirdRippleWidthAnimation = Tween<double>(begin: 10, end: 0).animate(
+      CurvedAnimation(
+        parent: thirdRippleController,
+        curve: Curves.ease,
+      ),
+    )..addListener(
+        () {
+          setState(() {});
+        },
+      );
+
+    centerCircleController = AnimationController(vsync: this, duration: const Duration(seconds: 1));
+
+    centerCircleRadiusAnimation = Tween<double>(begin: 35, end: 50).animate(
+      CurvedAnimation(
+        parent: centerCircleController,
+        curve: Curves.fastOutSlowIn,
+      ),
+    )
+      ..addListener(
+        () {
+          setState(() {});
+        },
+      )
+      ..addStatusListener(
+        (status) {
+          if (status == AnimationStatus.completed) {
+            centerCircleController.reverse();
+          } else if (status == AnimationStatus.dismissed) {
+            centerCircleController.forward();
+          }
+        },
+      );
+
+    firstRippleController.forward();
+    Timer(
+      const Duration(milliseconds: 765),
+      () => secondRippleController.forward(),
+    );
+
+    Timer(
+      const Duration(milliseconds: 1050),
+      () => thirdRippleController.forward(),
+    );
+
+    centerCircleController.forward();
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    firstRippleController.dispose();
+    secondRippleController.dispose();
+    thirdRippleController.dispose();
+    centerCircleController.dispose();
+    super.dispose();
+  }
+
   String info = '>_';
   int infoCode = 0;
 
@@ -43,37 +246,129 @@ class _HomePageState extends State<HomePage> {
 
     return Scaffold(
       body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              child: ListTile(
-                contentPadding: EdgeInsets.zero,
-                title: Text(
-                  'BFRBSys',
-                  textAlign: TextAlign.center,
-                  style: GoogleFonts.bebasNeue(fontSize: 50),
-                ),
-                subtitle: Text(
-                  'A wrist-worn device and monitoring system for a person with Body-focused Repetitive Behavior',
-                  textAlign: TextAlign.center,
-                  style: GoogleFonts.bebasNeue(fontSize: 20),
-                ),
-              ),
+        child: InkWell(
+          splashColor: Colors.transparent,
+          highlightColor: Colors.transparent,
+          onTap: () {
+            print('clicked123');
+          },
+          child: CustomPaint(
+            painter: MyPainter(
+              firstRippleRadiusAnimation.value,
+              firstRippleOpacityAnimation.value,
+              firstRippleWidthAnimation.value,
+              secondRippleRadiusAnimation.value,
+              secondRippleOpacityAnimation.value,
+              secondRippleWidthAnimation.value,
+              thirdRippleRadiusAnimation.value,
+              thirdRippleOpacityAnimation.value,
+              thirdRippleWidthAnimation.value,
+              centerCircleRadiusAnimation.value,
             ),
-            const SizedBox(height: 40),
-            ElevatedButton(
-                onPressed: () {
-                  Provider.of<ConnectionProvider>(context, listen: false).toggle(true);
-                },
-                child: Text(isBLEConnected ? 'Connected' : 'Connect')),
-            const SizedBox(height: 40),
-            Flexible(child: textInfo(info, infoCode)),
-          ],
+            child: Container(),
+          ),
         ),
+        // child: Column(
+        //   crossAxisAlignment: CrossAxisAlignment.center,
+        //   mainAxisAlignment: MainAxisAlignment.center,
+        //   children: [
+        //     Padding(
+        //       padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        //       child: ListTile(
+        //         contentPadding: EdgeInsets.zero,
+        //         title: Text(
+        //           'BFRBSys',
+        //           textAlign: TextAlign.center,
+        //           style: GoogleFonts.bebasNeue(fontSize: 50),
+        //         ),
+        //         subtitle: Text(
+        //           'A wrist-worn device and monitoring system for a person with Body-focused Repetitive Behavior',
+        //           textAlign: TextAlign.center,
+        //           style: GoogleFonts.bebasNeue(fontSize: 20),
+        //         ),
+        //       ),
+        //     ),
+        //     const SizedBox(height: 40),
+        //     ElevatedButton(
+        //         onPressed: () {
+        //           Provider.of<ConnectionProvider>(context, listen: false).toggle(true);
+        //         },
+        //         child: Text(isBLEConnected ? 'Connected' : 'Connect')),
+        //     const SizedBox(height: 40),
+        //     Flexible(child: textInfo(info, infoCode)),
+        //   ],
+        // ),
       ),
     );
+  }
+}
+
+class MyPainter extends CustomPainter {
+  final double firstRippleRadius;
+  final double firstRippleOpacity;
+  final double firstRippleStrokeWidth;
+  final double secondRippleRadius;
+  final double secondRippleOpacity;
+  final double secondRippleStrokeWidth;
+  final double thirdRippleRadius;
+  final double thirdRippleOpacity;
+  final double thirdRippleStrokeWidth;
+  final double centerCircleRadius;
+
+  MyPainter(
+      this.firstRippleRadius,
+      this.firstRippleOpacity,
+      this.firstRippleStrokeWidth,
+      this.secondRippleRadius,
+      this.secondRippleOpacity,
+      this.secondRippleStrokeWidth,
+      this.thirdRippleRadius,
+      this.thirdRippleOpacity,
+      this.thirdRippleStrokeWidth,
+      this.centerCircleRadius);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    Color myColor = Color(0xff653ff4);
+
+    Paint firstPaint = Paint();
+    firstPaint.color = myColor.withOpacity(firstRippleOpacity);
+    firstPaint.style = PaintingStyle.stroke;
+    firstPaint.strokeWidth = firstRippleStrokeWidth;
+
+    canvas.drawCircle(Offset(size.width / 2, size.height / 2), firstRippleRadius, firstPaint);
+
+    Paint secondPaint = Paint();
+    secondPaint.color = myColor.withOpacity(secondRippleOpacity);
+    secondPaint.style = PaintingStyle.stroke;
+    secondPaint.strokeWidth = secondRippleStrokeWidth;
+
+    canvas.drawCircle(Offset(size.width / 2, size.height / 2), secondRippleRadius, secondPaint);
+
+    Paint thirdPaint = Paint();
+    thirdPaint.color = myColor.withOpacity(thirdRippleOpacity);
+    thirdPaint.style = PaintingStyle.stroke;
+    thirdPaint.strokeWidth = thirdRippleStrokeWidth;
+
+    canvas.drawCircle(Offset(size.width / 2, size.height / 2), thirdRippleRadius, thirdPaint);
+
+    Paint fourthPaint = Paint();
+    fourthPaint.color = myColor;
+    fourthPaint.style = PaintingStyle.fill;
+
+    canvas.drawCircle(Offset(size.width / 2, size.height / 2), centerCircleRadius, fourthPaint);
+
+    TextSpan span = TextSpan(
+      style: GoogleFonts.bebasNeue(fontSize: 20),
+      text: 'Connect',
+    );
+    TextPainter tp = TextPainter(text: span, textAlign: TextAlign.left, textDirection: TextDirection.ltr);
+    tp.layout();
+    tp.paint(canvas, Offset(size.width / 2 - tp.width / 2, size.height / 2 - tp.height / 2));
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
   }
 }
