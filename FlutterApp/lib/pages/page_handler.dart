0a65cd611bba1e7642433acf62dbcb676bfbcc5d
Ryan Christopher Bahillo <christopherbahillo@gmail.com>
2023-10-31 21:27:16 +0800

    Remove AppBar, Drawer, and drawer items #13

--- FlutterApp/lib/pages/page_handler.dart
+++ FlutterApp/lib/pages/page_handler.dart
@@ -49,9 +49,8 @@ class _PageHandlerState extends State<PageHandler> {
   int currentPage = 0;
 
   late Future auth;
-
   late dynamic apiResponse;
-  int x = min(1, 2);
+
   @override
   void initState() {
     super.initState();
@@ -72,103 +71,6 @@ class _PageHandlerState extends State<PageHandler> {
     });
   }
 
-  Widget _createHeader() {
-    return DrawerHeader(
-        margin: EdgeInsets.zero,
-        padding: EdgeInsets.zero,
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            fit: BoxFit.cover,
-            image: AssetImage('images/ekusuuuu-calibaaaaaaaaaaaaa.png'),
-          ),
-        ),
-        child: Stack(children: const [
-          Positioned(
-              bottom: 12.0,
-              left: 16.0,
-              child: Text(
-                "xtnctx",
-                style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-              )),
-        ]));
-  }
-
-  Widget _createDrawerItem({
-    Icon? icon,
-    String? text,
-    bool selected = false,
-    Color? selectedColor,
-    GestureTapCallback? onTap,
-  }) {
-    return ListTile(
-      selected: selected,
-      selectedColor: selectedColor,
-      title: Row(
-        children: <Widget>[
-          icon!,
-          Padding(
-            padding: const EdgeInsets.only(left: 8.0),
-            child: Text(text!),
-          )
-        ],
-      ),
-      onTap: onTap,
-    );
-  }
-
-  Row _chartLegend() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        Wrap(
-          children: const [
-            Icon(
-              Icons.horizontal_rule_rounded,
-              color: CustomColor.lineXColor,
-            ),
-            Text('x'),
-          ],
-        ),
-        Wrap(
-          children: const [
-            Icon(
-              Icons.horizontal_rule_rounded,
-              color: CustomColor.lineYColor,
-            ),
-            Text('y'),
-          ],
-        ),
-        Wrap(
-          children: const [
-            Icon(
-              Icons.horizontal_rule_rounded,
-              color: CustomColor.lineZColor,
-            ),
-            Text('z'),
-          ],
-        )
-      ],
-    );
-  }
-
-  Widget _buildPopupDialog(BuildContext context, String title, Widget content) {
-    return AlertDialog(
-      title: Text(title),
-      content: content,
-      actions: <Widget>[
-        TextButton(
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-          child: const Text(
-            'Close',
-            style: TextStyle(color: Colors.blue),
-          ),
-        ),
-      ],
-    );
-  }
-
   void _onItemTapped(int index) {
     setState(() {
       currentPage = index;
@@ -176,88 +78,13 @@ class _PageHandlerState extends State<PageHandler> {
   }
 
   Widget _allowUserWidget() {
-    bool isConnected = Provider.of<ConnectionProvider>(context, listen: true).isConnected;
-
     return Scaffold(
       key: scaffoldKey,
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        actions: currentPage != 1
-            ? null
-            : [
-                Container(
-                  padding: const EdgeInsets.only(right: 25),
-                  child: MyTooltip(
-                      message: isConnected ? 'Connected' : 'Disconnected',
-                      child: Icon(
-                        Icons.rectangle,
-                        size: 10,
-                        color: isConnected ? Colors.greenAccent : Colors.redAccent,
-                      )),
-                ),
-                PopupMenuButton(
-                  tooltip: 'Show info',
-                  icon: const Icon(Icons.info_outline),
-                  itemBuilder: (context) => [
-                    // PopupMenuItem 1
-                    const PopupMenuItem(value: 1, child: Text('Chart Legend')),
-                    const PopupMenuItem(value: 2, child: Text('Sensor Range')),
-                  ],
-                  onSelected: (value) {
-                    if (value == 1) {
-                      showDialog(
-                        barrierColor: Theme.of(context).colorScheme.primaryContainer.withAlpha(125),
-                        context: context,
-                        builder: (BuildContext context, [_]) => _buildPopupDialog(
-                          context,
-                          'Chart Legend',
-                          Column(
-                            mainAxisSize: MainAxisSize.min,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Column(
-                                children: [
-                                  _chartLegend(),
-                                ],
-                              )
-                            ],
-                          ),
-                        ),
-                      );
-                    } else if (value == 2) {
-                      showDialog(
-                        barrierColor: Theme.of(context).colorScheme.primaryContainer.withAlpha(125),
-                        context: context,
-                        builder: (BuildContext context, [_]) => _buildPopupDialog(
-                          context,
-                          'Sensor Range',
-                          Column(
-                            mainAxisSize: MainAxisSize.min,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Column(
-                                children: const [
-                                  Text('Accelerometer : ±4 g'),
-                                  SizedBox(height: 5),
-                                  Text('Gyroscope : ±2000 dps'),
-                                  SizedBox(height: 5),
-                                  Text('Distance : 200 cm (max)'),
-                                  SizedBox(height: 5),
-                                  Text('Temperature : -70 to 382°C'),
-                                ],
-                              )
-                            ],
-                          ),
-                        ),
-                      );
-                    }
-                  },
-                ),
-              ],
-      ),
-      body: IndexedStack(
-        index: currentPage,
-        children: pages!,
+      body: SafeArea(
+        child: IndexedStack(
+          index: currentPage,
+          children: pages!,
+        ),
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
@@ -276,50 +103,6 @@ class _PageHandlerState extends State<PageHandler> {
         ],
         currentIndex: currentPage,
         onTap: _onItemTapped,
-      ),
-      drawerEdgeDragWidth: MediaQuery.of(context).size.width / 40,
-      drawerScrimColor: Theme.of(context).colorScheme.background.withAlpha(125),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            _createHeader(),
-            Column(
-              children: List.generate(pages!.length, (int index) {
-                dynamic curWidget = pages![index];
-                return _createDrawerItem(
-                  icon: currentPage == index ? curWidget.navBarIconSelected : curWidget.navBarIcon,
-                  text: curWidget.navBarTitle,
-                  selected: currentPage == index,
-                  selectedColor: Theme.of(context).colorScheme.primary,
-                  onTap: () {
-                    if (scaffoldKey!.currentState!.isDrawerOpen) {
-                      scaffoldKey!.currentState!.closeDrawer();
-                    }
-                    setState(() {
-                      currentPage = index;
-                    });
-                  },
-                );
-              }),
-            ),
-            const Divider(),
-            _createDrawerItem(
-                icon: const Icon(Icons.navigate_next),
-                text: 'Visit our site',
-                onTap: () {
-                  print(apiResponse);
-                }),
-            _createDrawerItem(icon: const Icon(Icons.read_more), text: 'Documentation'),
-            _createDrawerItem(icon: const Icon(Icons.face), text: 'Authors'),
-            const Divider(),
-            _createDrawerItem(icon: const Icon(Icons.coffee), text: 'Buy me a coffee'),
-            _createDrawerItem(icon: const Icon(Icons.bug_report), text: 'Bug report'),
-            const ListTile(
-              title: Text('v0.3.2', style: TextStyle(fontSize: 10)),
-            ),
-          ],
-        ),
       ),
     );
   }
