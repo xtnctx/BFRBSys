b5c86abdacfa17330da6327d2e817ebabe873592
Ryan Christopher Bahillo <christopherbahillo@gmail.com>
2024-02-04 03:41:00 +0800

    Add files via upload

--- FlutterApp/lib/custom_widgets/tooltip.dart
+++ FlutterApp/lib/custom_widgets/tooltip.dart
@@ -25,3 +25,42 @@ class MyTooltip extends StatelessWidget {
     tooltip?.ensureTooltipVisible();
   }
 }
+
+class BackgroundDesignL extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.lineTo(0, 80);
+    path.quadraticBezierTo(size.width / 4, 30, size.width / 2, 45);
+    path.quadraticBezierTo(3 / 4 * size.width, 60, size.width, 15);
+    path.lineTo(size.width, 0);
+
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return false;
+  }
+}
+
+class BackgroundDesignR extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.moveTo(size.width, 0);
+    path.lineTo(size.width, 80);
+    path.quadraticBezierTo(3 / 4 * size.width, 30, size.width / 2, 45);
+    path.quadraticBezierTo(size.width / 4, 60, 0, 15);
+    path.lineTo(0, 0);
+
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return false;
+  }
+}
